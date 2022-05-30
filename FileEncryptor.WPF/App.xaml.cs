53acc6c16f7d9d35f9b87baf430ecf57e9be20a9
Ynychar1980 <yanychar1980@gmail.com>
2022-05-30 14:32:54 +0300

    IoC

--- FileEncryptor.WPF/App.xaml.cs
+++ FileEncryptor.WPF/App.xaml.cs
@@ -1,11 +1,35 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace FileEncryptor.WPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        private static IHost __Host;
+
+        public static IHost Host => __Host 
+            ??= Program.CreaHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+
+            base.OnStartup(e);
+
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            using (Host) await Host.StopAsync();
+        }
     }
 }
---
+++ FileEncryptor.WPF/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+
+namespace FileEncryptor.WPF
+{
+    internal static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        public static IHostBuilder CreaHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices(App.ConfigureServices);
+    }
+}
