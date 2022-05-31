3eaf17ca1644f0bd1389df37cf9e62b770a2289b
Ynychar1980 <yanychar1980@gmail.com>
2022-05-31 07:56:36 +0300

    Диалог с пользователем по выбору файла

--- FileEncryptor.WPF/Services/Interfaces/IUserDialog.cs
+++ FileEncryptor.WPF/Services/Interfaces/IUserDialog.cs
@@ -1,6 +1,12 @@
-﻿namespace FileEncryptor.WPF.Services.Interfaces
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace FileEncryptor.WPF.Services.Interfaces
 {
     internal interface IUserDialog
     {
+        bool OpenFile(string Title, out string SelectedFile, string Filter = "Все файлы (*.*)|*.*");
+
+        bool OpenFiles(string Title, out IEnumerable<string> SelectedFiles, string Filter = "Все файлы (*.*)|*.*");
     }
 }
